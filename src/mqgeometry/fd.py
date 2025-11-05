57be2fea4c78bcbeafe33e4633ba3e177f73013f
grigaut <gaetanrigaut.s@gmail.com>
2025-11-05 10:33:18 +0100

    Implement non-padded laplacian

--- src/mqgeometry/fd.py
+++ src/mqgeometry/fd.py
@@ -1,16 +1,31 @@
+import torch
 import torch.nn.functional as F
+
 
 def grad_perp(f, dx, dy):
     """Orthogonal gradient"""
-    return (f[...,:-1] - f[...,1:]) / dy, (f[...,1:,:] - f[...,:-1,:]) / dx
+    return (f[..., :-1] - f[..., 1:]) / dy, (f[..., 1:, :] - f[..., :-1, :]) / dx
 
 
 def interp_TP(f):
-    return 0.25 *(f[...,1:,1:] + f[...,1:,:-1] + f[...,:-1,1:] + f[...,:-1,:-1])
+    return 0.25 * (
+        f[..., 1:, 1:] + f[..., 1:, :-1] + f[..., :-1, 1:] + f[..., :-1, :-1]
+    )
+
+
+def laplacian(f: torch.Tensor, dx: float, dy: float) -> torch.Tensor:
+    """Non-padded laplacian."""
+    return (
+        (f[..., 2:, 1:-1] + f[..., :-2, 1:-1] - 2 * f[..., 1:-1, 1:-1]) / dx**2
+        + (f[..., 1:-1, 2:] + f[..., 1:-1, :-2] - 2 * f[..., 1:-1, 1:-1]) / dy**2,
+    )
 
 
 def laplacian_h(f, dx, dy):
     return F.pad(
-        (f[...,2:,1:-1] + f[...,:-2,1:-1] - 2*f[...,1:-1,1:-1]) / dx**2 \
-      + (f[...,1:-1,2:] + f[...,1:-1,:-2] - 2*f[...,1:-1,1:-1]) / dy**2,
-        (1,1,1,1), mode='constant', value=0.)
+        (f[..., 2:, 1:-1] + f[..., :-2, 1:-1] - 2 * f[..., 1:-1, 1:-1]) / dx**2
+        + (f[..., 1:-1, 2:] + f[..., 1:-1, :-2] - 2 * f[..., 1:-1, 1:-1]) / dy**2,
+        (1, 1, 1, 1),
+        mode="constant",
+        value=0.0,
+    )
