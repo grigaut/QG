b23364589177a84a88fdfa3439b26b0d455413e3
grigaut <gaetanrigaut.s@gmail.com>
2026-04-01 14:18:14 +0200

    Update plots

--- src/qg/plots/__init__.py
+++ src/qg/plots/__init__.py
@@ -1,11 +1,27 @@
 """Plotting related tools."""
 
 from qg.plots.plt_wrapper import (
+    blittable_suptitle,
+    clamp_ylims,
+    close,
     imshow,
+    imshow_,
     set_coltitles,
     set_rowtitles,
+    set_ylims,
     show,
     subplots,
 )
 
-__all__ = ["imshow", "set_coltitles", "set_rowtitles", "show", "subplots"]
+__all__ = [
+    "blittable_suptitle",
+    "clamp_ylims",
+    "close",
+    "imshow",
+    "imshow_",
+    "set_coltitles",
+    "set_rowtitles",
+    "set_ylims",
+    "show",
+    "subplots",
+]
